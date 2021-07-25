c378e900a854090ed85f67c7ed6553f84beb1780
JULIO-ARIAS-C <julio.arias@udla.edu.ec>
2021-07-25 08:34:28 -0500

    Comit01

--- group_app/urls.py
+++ group_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('main',views.main),
     path('create_user',views.create_user),
     path('create_org', views.create_org),
-    path('/groups/<int:organization_id>',views.view_group),
+    path('groups/<int:organization_id>',views.view_group),
     path('groups', views.groups),
     path('login',views.login),
     path('logout',views.logout),
